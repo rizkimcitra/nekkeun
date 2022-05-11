fc425e14c9c041603248b60a863e4552dd716269
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-05-11 21:29:13 +0700

    style: modify UI

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,23 @@
+import { clsxm } from '@/libs'
+
 import type { NextPage } from 'next'
 import Head from 'next/head'
 
 const Home: NextPage = () => {
   return (
-    <div className='max-w-2xl py-10 mx-auto w-[90%]'>
+    <main className={clsxm('layout')}>
       <Head>
         <title>Nekkeun - NEXT.js App</title>
       </Head>
-      <h1>Hello World</h1>
-    </div>
+
+      <section className={clsxm('min-h-screen', 'flex flex-col', 'items-center justify-center')}>
+        <h1 className={clsxm('mb-4', 'text-center', 'md:text-5xl')}>Nekkeun - NEXT.js App</h1>
+        <p className='text-center max-w-prose'>
+          Nekkeun doesn&apos;t mean <em>ngabatin</em>, Nekken mean <em>Nekkeun</em>, in other word,
+          it&apos;s <em>NextKeun</em>, which means let&apos;s build an app with NEXT.js
+        </p>
+      </section>
+    </main>
   )
 }
 
